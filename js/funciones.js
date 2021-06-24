65c04f6205291fa240e6c06f3aea46866d9d0723
Mario Angel López Esquivel <mario_angel99@hotmail.com>
2021-06-24 16:17:47 -0500

    Versión nueva 1.1 de CV

--- js/funciones.js
+++ js/funciones.js
@@ -4,12 +4,24 @@ window.addEventListener('scroll',function()
     let InfMario = this.document.getElementById('infoMia');
     let btnBack = this.document.getElementById('btnBack');
 
+
+    let portafolio = this.document.getElementById('divPortafolio');
+    
     let pos = fotoMario.getBoundingClientRect().top;
-    console.log(pos);
+    let pos2 = portafolio.getBoundingClientRect().top;
 
 
     let botonHome = window.innerHeight/1;
-
+    let limite = window.innerHeight/0.8;
+    if(pos2 < limite)
+    {
+        portafolio.style.transform="translateY(0%)"
+        portafolio.style.opacity="1"
+    }
+    else{
+        portafolio.style.transform="translateY(50%)"
+        portafolio.style.opacity="0"
+    }
     if(pos < botonHome)
     {
         fotoMario.style.opacity="1";
@@ -24,6 +36,6 @@ window.addEventListener('scroll',function()
         fotoMario.style.transform="translateY(50%)"
         InfMario.style.opacity="0";
         InfMario.style.transform="translateY(50%)"
-        btnBack.style.transform="translateY(180%)"
+        btnBack.style.transform="translateY(280%)"
     }
 })
